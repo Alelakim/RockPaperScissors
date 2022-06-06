d73390e0996b774f852bc6dbe84a82001e1f17e0
mikaela.westring <mikaela.westring@voyado.com>
2022-06-06 07:56:52 +0200

    remove and sort using

--- RockPaperScissors/Models/Result.cs
+++ RockPaperScissors/Models/Result.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RockPaperScissors.Models
+﻿namespace RockPaperScissors.Models
 {
     public class Result
     {
--- RockPaperScissors/Services/GameService.cs
+++ RockPaperScissors/Services/GameService.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RockPaperScissors.Models;
-using System.Net;
 
 namespace RockPaperScissors.Services
 {
